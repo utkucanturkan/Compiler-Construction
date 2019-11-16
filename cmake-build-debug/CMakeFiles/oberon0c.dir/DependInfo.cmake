41e65888513b05cb2084a6278da5b35b791a4228
utkucanturkan <utkucanturkan@gmail.com>
2019-11-16 23:50:39 +0100

    Initial Commit
    Construction a Oberon compiler

---
+++ cmake-build-debug/CMakeFiles/oberon0c.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/grossniklaus/Desktop/oberon0c/main.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/main.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/parser/Parser.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/parser/Parser.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/scanner/IdentToken.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/scanner/IdentToken.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/scanner/NumberToken.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/scanner/NumberToken.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/scanner/Scanner.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/scanner/Scanner.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/scanner/StringToken.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/scanner/StringToken.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/scanner/Token.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/scanner/Token.cpp.o"
+  "/Users/grossniklaus/Desktop/oberon0c/util/Logger.cpp" "/Users/grossniklaus/Desktop/oberon0c/cmake-build-debug/CMakeFiles/oberon0c.dir/util/Logger.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../scanner"
+  "../util"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/oberon0c.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oberon0c.dir/scanner/IdentToken.cpp.o"
+  "CMakeFiles/oberon0c.dir/scanner/NumberToken.cpp.o"
+  "CMakeFiles/oberon0c.dir/scanner/Scanner.cpp.o"
+  "CMakeFiles/oberon0c.dir/scanner/StringToken.cpp.o"
+  "CMakeFiles/oberon0c.dir/scanner/Token.cpp.o"
+  "CMakeFiles/oberon0c.dir/util/Logger.cpp.o"
+  "CMakeFiles/oberon0c.dir/main.cpp.o"
+  "CMakeFiles/oberon0c.dir/parser/Parser.cpp.o"
+  "oberon0c.pdb"
+  "oberon0c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oberon0c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
