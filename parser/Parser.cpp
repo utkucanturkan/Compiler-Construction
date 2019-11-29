2e200f7e1df0a1a7dd3f93337d572707b003584f
utkucanturkan <utkucanturkan@gmail.com>
2019-11-30 00:18:17 +0100

    Continue on Oberon0 parser

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -36,13 +36,9 @@ const Node* Parser::module()
 
 	std::string name;
 	bool isEnd = false;
-	bool shouldProceed = false;
-	TokenType nextTokenType;
-	if (scanner_->peekToken()->getType() == TokenType::kw_module)
-	{
-		shouldProceed = true;
-		nextTokenType = TokenType::const_ident;
-	}
+	bool shouldProceed = true;
+	TokenType nextTokenType = TokenType::kw_module;
+
 	while (shouldProceed)
 	{
 		Token token = *scanner_->nextToken();
@@ -50,6 +46,9 @@ const Node* Parser::module()
 		{
 			switch (nextTokenType)
 			{
+			case TokenType::kw_module:
+				nextTokenType = TokenType::const_ident;
+				break;
 			case TokenType::const_ident:
 				if (isEnd) {
 					if (false /*name != token.getValue()*/) {
@@ -84,10 +83,14 @@ const Node* Parser::module()
 				shouldProceed = false;
 				break;
 			default:
-				logger_->error(token.getPosition() ," - Syntax Error");
+				logger_->error(token.getPosition(), " - Syntax Error");
 				shouldProceed = false;
 				break;
 			}
+		}
+		else {
+			logger_->error(token.getPosition(), " - Syntax Error");
+			shouldProceed = false;
 		}
 	}
 	return nullptr;
@@ -120,28 +123,131 @@ const Node* Parser::declarations() {
 		procedure_declaration();
 		break;
 	default:
+		logger_->error(token.getPosition(), " - Syntax Error");
 		break;
-	}
-
-	for (int i = 1; i <= 5; i++)
-	{
-		scanner_->nextToken();
 	}
 	return nullptr;
 }
 const Node* Parser::const_declarations() {
-	scanner_->peekToken();
 	/*
-	"CONST" {ident "=" expression ";"}
+		"CONST" {ident "=" expression ";"}
 	*/
+	;
+	bool shouldProceed = true;
+	TokenType nextToken = TokenType::kw_const;
+	while (shouldProceed)
+	{
+		Token token = *scanner_->nextToken();
+		if (token.getType() == nextToken)
+		{
+			switch (nextToken)
+			{
+			case TokenType::kw_const:
+				nextToken = TokenType::const_ident;
+				break;
+			case TokenType::const_ident:
+				nextToken = TokenType::op_eq;
+				break;
+			case TokenType::op_eq:
+				expression();
+				nextToken = TokenType::semicolon;
+				break;
+			case TokenType::semicolon:
+				shouldProceed = false;
+				break;
+			default:
+				logger_->error(token.getPosition(), " - Syntax Error");
+				shouldProceed = false;
+				break;
+			}
+		}
+		else {
+			logger_->error(token.getPosition(), " - Syntax Error");
+			shouldProceed = false;
+		}
+	}
+
 	return nullptr;
 }
 
 const Node* Parser::type_declarations() {
+	/*
+		"TYPE" {ident "=" type ";"}
+	*/
+	bool shouldProceed = true;
+	TokenType nextToken = TokenType::kw_type;
+	while (shouldProceed)
+	{
+		Token token = *scanner_->nextToken();
+		if (token.getType() == nextToken)
+		{
+			switch (nextToken)
+			{
+			case TokenType::kw_type:
+				nextToken = TokenType::const_ident;
+				break;
+			case TokenType::const_ident:
+				nextToken = TokenType::op_eq;
+				break;
+			case TokenType::op_eq:
+				type();
+				nextToken = TokenType::semicolon;
+				break;
+			case TokenType::semicolon:
+				shouldProceed = false;
+				break;
+			default:
+				logger_->error(token.getPosition(), " - Syntax Error");
+				shouldProceed = false;
+				break;
+			}
+		}
+		else {
+			logger_->error(token.getPosition(), " - Syntax Error");
+			shouldProceed = false;
+		}
+	}
 	return nullptr;
 }
 
 const Node* Parser::var_declarations() {
+	/*
+		["VAR" {IdentList ":" type ";"}]
+		IdentList = ident {"," ident}
+	*/
+	bool shouldProceed = true;
+	TokenType nextToken = TokenType::kw_var;
+	while (shouldProceed)
+	{
+		Token token = *scanner_->nextToken();
+		if (token.getType() == nextToken)
+		{
+			switch (nextToken)
+			{
+			case TokenType::kw_var:
+				nextToken = TokenType::const_ident;
+				break;
+			case TokenType::const_ident:
+				token = *scanner_->nextToken();
+				if (token.getType() == TokenType::comma)
+				{
+					nextToken = TokenType::const_ident;
+				}
+				else if (token.getType() == TokenType::colon) {
+					type();
+					nextToken = TokenType::semicolon;
+				}
+				break;
+			case TokenType::semicolon:
+				shouldProceed = false;
+				break;
+			default:
+				logger_->error(token.getPosition(), " - Syntax Error");
+				shouldProceed = false;
+				break;
+			}
+		}
+	}
 	return nullptr;
 }
 
@@ -149,11 +255,71 @@ const Node* Parser::procedure_declaration() {
 	return nullptr;
 }
 
-const Node* Parser::statement_sequence() {
-	for (int i = 1; i <= 5; i++)
+const Node* Parser::expression() {
+	/*
+	SimpleExpression [("=" | "#" | "<" | "<=" | ">" | ">=") SimpleExpression]
+	*/
+	simple_expression();
+	Token token = *scanner_->nextToken();
+	if (token.getType() == TokenType::op_eq || token.getType() == TokenType::op_neq || token.getType() == TokenType::op_lt || token.getType() == TokenType::op_leq || token.getType() == TokenType::op_gt || token.getType() == TokenType::op_geq)
 	{
-		scanner_->nextToken();
+		simple_expression();
 	}
+	else {
+		logger_->error(token.getPosition(), " - Syntax Error");
+	}
+	return nullptr;
+}
+
+const Node* Parser::simple_expression() {
+	Token token = *scanner_->nextToken();
+	if (token.getType() == TokenType::op_plus || token.getType() == TokenType::op_minus)
+	{
+		term();
+		token = *scanner_->nextToken();
+		if (token.getType() == TokenType::op_plus || token.getType() == TokenType::op_minus || token.getType() == TokenType::op_or)
+		{
+			term();
+		}
+		else {
+			logger_->error(token.getPosition(), " - Syntax Error");
+		}
+	}
+	else {
+		logger_->error(token.getPosition(), " - Syntax Error");
+	}
+	return nullptr;
+}
+
+const Node* Parser::term() {
+	/*
+	factor {("*" | "DIV" | "MOD" | "&") factor}
+	*/
+	factor();
+	Token token = *scanner_->nextToken();
+	if (token.getType() == TokenType::op_times || token.getType() == TokenType::op_div || token.getType() == TokenType::op_mod || token.getType() == TokenType::op_and)
+	{
+		factor();
+	}
+	return nullptr;
+}
+const Node* Parser::factor() {
+	/*
+	ident selector | integer | "(" expression ")" | "~" factor
+	*/
+	Token token = *scanner_->nextToken();
+	if (token.getType() == TokenType::const_ident)
+	{
+		
+	}
+	return nullptr;
+}
+
+const Node* Parser::type() {
+	return nullptr;
+}
+
+const Node* Parser::statement_sequence() {
 	return nullptr;
 }
 
